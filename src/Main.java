50825e6f815491ea2e5882dd8df3484ba36d59ce
kimmunju-1130 <k22qqaa@gmail.com>
2025-01-13 21:30:59 +0900

    Merge branch 'main' of https://github.com/kimmunju-1130/final

--- src/Main.java
+++ src/Main.java
@@ -26,31 +26,31 @@ class Student {
     private int age;
     private int number;
     private ArrayList<Subject> subjects;
-    private String St_pw;
-    private String St_regist;
+    private String studentPassword;
+    private String studentRegist;
 
-    Student (String name, int age, int number, ArrayList<Subject> subjects, String St_pw, String St_regist) {
+    Student (String name, int age, int number, ArrayList<Subject> subjects, String studentPassword, String studentRegist) {
         this.name = name;
         this.age = age;
         this.number = number;
         this.subjects = subjects;
-        this.St_pw = St_pw;
-        this.St_regist = St_regist;
+        this.studentPassword = studentPassword;
+        this.studentRegist = studentRegist;
     }
 
     public String getName() {return name;}
     public int getAge() {return age;}
     public int getNumber() {return number;}
     public ArrayList<Subject> getSubjects() {return subjects;}
-    public String getStpw() {return St_pw;}
-    public String getSt_regist() {return St_regist;}
+    public String getstudentPassword() {return studentPassword;}
+    public String getstudentRegist() {return studentRegist;}
 
     public void setName(String name){this.name = name;}
     public void setAge(int age){this.age = age;}
     public void setNumber(int number){this.number = number;}
     public void setSubjects(ArrayList<Subject> subject){this.subjects = subject;}
-    public void setStpw(String St_pw) {this.St_pw = St_pw;}
-    public void setSt_regist(String St_regist) {this.St_regist = St_regist;}
+    public void setstudentPassword(String studentPassword) {this.studentPassword = studentPassword;}
+    public void setstudentRegist(String studentRegist) {this.studentRegist = studentRegist;}
 
     public String toString() { // 형변환
 
@@ -58,50 +58,49 @@ class Student {
     }
 
 }
+class Professor {
+    private String professorName;
+    private String professorMajor;
+    private int professorNumber;
+    private String professorPassword;
 
-class professor {
-    private String pf_name;
-    private String pf_major;
-    private int pf_number;
-    private String pf_pw;
-
-    professor(String pf_name, String pf_major, int pf_number,String pf_pw){
-        this.pf_name = pf_name;
-        this.pf_major = pf_major;
-        this.pf_number = pf_number;
-        this.pf_pw = pf_pw;
+    Professor(String professorName, String professorMajor, int professorNumber,String professorPassword){
+        this.professorName = professorName;
+        this.professorMajor = professorMajor;
+        this.professorNumber = professorNumber;
+        this.professorPassword = professorPassword;
     }
 
-    public String getNamepf() { return pf_name;}
-    public String getMajorpf() { return pf_major;}
-    public int getNumberpf() { return pf_number;}
-    public String getPfpw() { return pf_pw;}
+    public String getNamepf() { return professorName;}
+    public String getMajorpf() { return professorMajor;}
+    public int getNumberpf() { return professorNumber;}
+    public String getPfpw() { return professorPassword;}
 
-    public void setNamepf(String pf_name) { this.pf_name = pf_name;}
-    public void setMajorpf(String pf_major) { this.pf_major = pf_major;}
-    public void setNumberpf(int pf_number) { this.pf_number = pf_number;}
-    public void setPfpw(String pf_pw) { this.pf_pw = pf_pw;}
+    public void setNamepf(String professorName) { this.professorName = professorName;}
+    public void setMajorpf(String professorMajor) { this.professorMajor = professorMajor;}
+    public void setNumberpf(int professorNumber) { this.professorNumber = professorNumber;}
+    public void setPfpw(String professorPassword) { this.professorPassword = professorPassword;}
 }
 
 
 public class Main {
-    private static ArrayList<Student> StudentArray = new ArrayList<>();
-    private static ArrayList<professor> professorArray = new ArrayList<>();
-    private static ArrayList<Subject> SubjectArray = new ArrayList<>();
-    private static ArrayList<Subject> SubjectArrayB = new ArrayList<>();
+    private static ArrayList<Student> studentArray = new ArrayList<>();
+    private static ArrayList<Professor> professorArray = new ArrayList<>();
+    private static ArrayList<Subject> subjectArray = new ArrayList<>();
+    private static ArrayList<Subject> subjectArrayB = new ArrayList<>();
     private static ArrayList<Integer> LoaArray = new ArrayList<>();
     private static Scanner sc = new Scanner(System.in);
-    private static boolean pf_state = false;
-    private static boolean st_state = false;
+    private static boolean professorState = false;
+    private static boolean studentState = false;
 
     public static void Inithial(){
-        professorArray.add(new professor("짱구", "코딩", 1234, "4321"));
-        StudentArray.add(new Student("유리", 28, 12345, SubjectArray, "0987", "재학"));//어레이리스트로 넣어야함
-        StudentArray.add(new Student("맹구", 4, 1233, SubjectArrayB, "009988", "재학"));
-        SubjectArray.add(new Subject("코딩", 10));
-        SubjectArrayB.add(new Subject("코딩", 20));
+        professorArray.add(new Professor("짱구", "코딩", 1234, "4321"));
+        studentArray.add(new Student("유리", 28, 12345, subjectArray, "0987", "재학"));//어레이리스트로 넣어야함
+        studentArray.add(new Student("맹구", 4, 1233, subjectArrayB, "009988", "재학"));
+        subjectArray.add(new Subject("코딩", 10));
+        subjectArrayB.add(new Subject("코딩", 20));
     }
-    public static void RunApp() {
+    public static void runApp() {
         System.out.println("아이디와 비밀번호를 입력하시오.");
         int id = sc.nextInt();
         sc.nextLine();
@@ -109,26 +108,27 @@ public class Main {
         System.out.println(id);
         System.out.println(pw);
 
-        for (Student s : StudentArray)
+        for (Student s : studentArray)
             if (id == s.getNumber()) {
-                if (pw.equals(s.getStpw())) {
+                if (pw.equals(s.getstudentPassword())) {
                     System.out.println("로그인이 되었습니다.");
-                    st_state = true;
-                    while(st_state) { Student_menu(id);}
+                    studentState = true;
+                    while(studentState) { studentMenu(id);}
                 } else { System.out.println("아이디와 비밀번호가 틀렸습니다."); }
             }
-        for (professor p : professorArray)
+        for (Professor p : professorArray)
             if (id == p.getNumberpf()) {
                 if (pw.equals(p.getPfpw())) {
                     System.out.println("로그인이 되었습니다.");
-                    pf_state = true;
-                    while(pf_state) {
-                        ProfessorMenu(id);
+                    
+                    professorState = true;
+                    while(professorState) {
+                        professorMenu(id);
                     }
                 } else { System.out.println("아이디와 비밀번호가 틀렸습니다.");}
             }
     }
-    public static void ProfessorMenu(int id) {
+    public static void professorMenu(int id) {
         System.out.println("숫자를 입력하시오.");
         System.out.println("1. 학생추가"); //스캔받아서 추가 하고
         System.out.println("2. 학생삭제");
@@ -140,19 +140,19 @@ public class Main {
         System.out.println("8. 로그아웃");
         int select = sc.nextInt();
         switch(select) {
-            case 1: StudentAdd(); break;
-            case 2 : StudentDelete(); break;
-            case 3 : StudentCheck(); break;
-            case 4 : AllstudentCheck(); break;
-            case 5 : StudentChange(); break;
-            case 6 : StudentgradeInput(); break;
-            case 7 : StudentloaCheck(); break;
-            case 8 : ProfessorLogout(); break;
+            case 1: studentAdd(); break;
+            case 2 : studentDelete(); break;
+            case 3 : studentCheck(); break;
+            case 4 : allStudentCheck(); break;
+            case 5 : studentChange(); break;
+            case 6 : studentGradeInput(); break;
+            case 7 : studentLoaCheck(); break;
+            case 8 : logout(); break;
         }
-        RunApp();
+
     }
 
-    public static void StudentAdd() {
+    public static void studentAdd() {
         System.out.println("이름을 입력해주세요");
         sc.nextLine();
         String nam = sc.nextLine();
@@ -165,21 +165,21 @@ public class Main {
         sc.nextLine();
         String sp = sc.nextLine();
         Student A = new Student(nam, ag, number, null, sp, "재학");
-        StudentArray.add(A);
-        //System.out.println(StudentArray);
+        studentArray.add(A);
+        //System.out.println(studentArray);
     }
-    public static void StudentDelete() {
+    public static void studentDelete() {
         System.out.println("삭제할 학번을 입력하시오.");
         int numb = sc.nextInt();
-        for (int i = 0; i < StudentArray.size(); i++) {
-            if (StudentArray.get(i).getNumber() == numb) {
-                StudentArray.remove(i);
+        for (int i = 0; i < studentArray.size(); i++) {
+            if (studentArray.get(i).getNumber() == numb) {
+                studentArray.remove(i);
                 System.out.println("삭제되었습니다!.");
             }
         }
     }
-    public static void StudentCheck() {
-        for (Student s : StudentArray) {
+    public static void studentCheck() {
+        for (Student s : studentArray) {
             System.out.println("이름, 나이를 조회할 학번을 입력하시오.");
             int numb = sc.nextInt();
             if (s.getNumber() == numb) {
@@ -189,20 +189,20 @@ public class Main {
         }
     }
 
-    public static void AllstudentCheck() {
-        if (StudentArray.isEmpty()) { // .isEmtpy() 비어있으면 true, 하나라도 값이 있으면 false 반환
+    public static void allStudentCheck() {
+        if (studentArray.isEmpty()) { // .isEmtpy() 비어있으면 true, 하나라도 값이 있으면 false 반환
             System.out.println("학생이 없습니다.");
         }
         else {
-            for (Student s : StudentArray) {
+            for (Student s : studentArray) {
                 System.out.println(s.getName());
             }
         }
     }
-    public static void StudentChange() {
+    public static void studentChange() {
         System.out.println("변경할 학번을 입력하시오.");
         int numb = sc.nextInt();
-        for (Student s : StudentArray) {
+        for (Student s : studentArray) {
             if (s.getNumber() == numb) {
                 System.out.println("변경할 이름을 입력하시오");
                 sc.nextLine();
@@ -211,14 +211,14 @@ public class Main {
                 int age = sc.nextInt();
                 s.setName(name);
                 s.setAge(age);
-                //System.out.println(StudentArray);
+                //System.out.println(studentArray);
             }
         }
     }
-    public static void StudentgradeInput() {
+    public static void studentGradeInput() {
         System.out.println("성적을 입력할 학번을 입력하시오.");
         int numb = sc.nextInt();
-        for (Student s : StudentArray) {
+        for (Student s : studentArray) {
             if (s.getNumber() == numb) {
                 System.out.println("성적을 입력하시오.");
                 int scgrade = sc.nextInt();
@@ -227,11 +227,11 @@ public class Main {
                     s.setSubjects(new ArrayList<>());
                 }
                 s.getSubjects().add(A);
-                System.out.println(StudentArray);
+                System.out.println(studentArray);
             }
         }
     }
-    public static void StudentloaCheck() {
+    public static void studentLoaCheck() {
         System.out.println("휴학 신청 학생을 조회합니다.");
         for (int i = 0; i < LoaArray.size(); i++) {
             System.out.printf("[%s]\t", LoaArray.get(i));
@@ -241,22 +241,22 @@ public class Main {
         System.out.println("1. 신청생 휴학 적용");
         int numb = sc.nextInt();
         if(numb ==1) {
-            for (Student s : StudentArray) {
+            for (Student s : studentArray) {
                 for (int j = 0; j < LoaArray.size(); j++) {
                     if (s.getNumber() == LoaArray.get(j)) {
-                        s.setSt_regist("휴학");
-                        System.out.println("이름: " + s.getName() + " " + s.getSt_regist() + "처리됐습니다.");
+                        s.setstudentRegist("휴학");
+                        System.out.println("이름: " + s.getName() + " " + s.getstudentRegist() + "처리됐습니다.");
                     }
                 }
             }
         }
     }
-    public static void ProfessorLogout() {
-        pf_state = false;
+    public static void logout() {
         System.out.println("로그아웃되었습니다.");
+        runApp();
     }
 
-    public static void Student_menu(int id) {
+    public static void studentMenu(int id) {
         System.out.println("숫자를 입력하시오.");
         System.out.println("1. 성적 조회");
         System.out.println("2. 비밀번호 변경");
@@ -265,16 +265,15 @@ public class Main {
         int select = sc.nextInt();
 
         switch(select) {
-            case 1 : Gradecheck(id); break;
-            case 2 : StpwChange(id); break;
-            case 3 : LoaApply(id); break;
-            case 4 : StudentLogout(); break;
+            case 1 : gradecheck(id); break;
+            case 2 : studentPasswordChange(id); break;
+            case 3 : loaApply(id); break;
+            case 4 : logout(); break;
         }
-        RunApp();
 
     }
-    public static void Gradecheck(int id) {
-        for (Student s : StudentArray) {
+    public static void gradecheck(int id) {
+        for (Student s : studentArray) {
             if (s.getNumber() == id) {
                 s.getSubjects();
                 System.out.println(s.getSubjects());
@@ -282,18 +281,18 @@ public class Main {
             else {System.out.println("해당 학번이 없습니다.");}
         }
     }
-    public static void StpwChange(int id) {
-        for (Student s : StudentArray) {
+    public static void studentPasswordChange(int id) {
+        for (Student s : studentArray) {
             if (s.getNumber() == id) {
                 System.out.println("변경할 비밀번호를 입력하시오");
                 sc.nextLine();
                 String changepw = sc.nextLine();
-                s.setStpw(changepw);
-                System.out.println("비밀번호가 " + s.getStpw() + " (으)로 변경되었습니다.");
+                s.setstudentPassword(changepw);
+                System.out.println("비밀번호가 " + s.getstudentPassword() + " (으)로 변경되었습니다.");
             }
         }
     }
-    public static void LoaApply(int id) {
+    public static void loaApply(int id) {
         System.out.println("휴학신청 하시겠습니까? 1.예 2.메뉴로 돌아가기 ");
         int LoaSelect = sc.nextInt();
         if(LoaSelect==1) {
@@ -305,13 +304,9 @@ public class Main {
             System.out.println("메뉴로 돌아갑니다.");
         }
     }
-    public static void StudentLogout() {
-        st_state = false;
-        System.out.println("로그아웃되었습니다.");
-    }
     public static void main(String[] args) {
         Inithial();
-        RunApp();
+        runApp();
         }
     }
 
